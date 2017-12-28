ce407d7e0f842b925fc96dc509864179804e8b28
Henning Langhorst <henning.langhorst@googlemail.com>
2017-12-28 22:11:50 +0100

    HTTP requests

--- src/main/kotlin/de/henninglanghorst/kotlinstuff/http/HttpClient.kt
+++ src/main/kotlin/de/henninglanghorst/kotlinstuff/http/HttpClient.kt
@@ -7,52 +7,46 @@ import kotlin.streams.asSequence
 
 
 fun main(args: Array<String>) {
-    val post = post(
-            "https://postman-echo.com/post?hhhhh=5",
-            "Basic d2lraTpwZWRpYQ==",
-            json(6, "Miller"))
+    val post = httpRequest("https://postman-echo.com/post?hhhhh=5") {
+        requestMethod = "POST"
+        setRequestProperty("Authorization", "Basic d2lraTpwZWRpYQ==")
+        setRequestProperty("Content-Type", "application/json; charset=utf-8")
+        body = json(6, "Miller")
+    }
     println(post)
 
-    val get = get("https://postman-echo.com/get?s=5&aaa=43", "Basic d2lraTpwZWRpYQ==")
+    val get = httpRequest("https://postman-echo.com/get?s=5&aaa=43") {
+        requestMethod = "GET"
+        setRequestProperty("Authorization", "Basic d2lraTpwZWRpYQ==")
+    }
 
     println(get)
+
 }
 
 private fun json(id: Int, name: String) = """{"id": ${id}, "name": "${name}"}"""
 
-private fun post(url: String, authorization: String? = null, json: String = "") =
+private fun httpRequest(url: String, setup: HttpURLConnection.() -> Unit) =
         URL(url).openConnection()
                 .let { it as HttpURLConnection }
+                .apply { doInput = true }
+                .apply(setup)
                 .run {
-                    requestMethod = "POST"
-                    doOutput = true
-                    doInput = true
-                    authorization?.run { setRequestProperty("Authorization", authorization) }
-                    setRequestProperty("Content-Type", "application/json; charset=utf-8")
-
-                    DataOutputStream(outputStream).use {
-                        it.writeUTF(json)
-                        it.flush()
-                    }
                     inputStream.bufferedReader(Charsets.UTF_8)
                             .lines()
                             .asSequence()
                             .reduce { acc, s -> acc + s }
+
                 }
 
-private fun get(url: String, authorization: String? = null) =
-        URL(url).openConnection()
-                .let { it as HttpURLConnection }
-                .run {
-                    requestMethod = "GET"
-                    doOutput = false
-                    doInput = true
-                    authorization?.run { setRequestProperty("Authorization", authorization) }
-                    setRequestProperty("Content-Type", "application/json; charset=utf-8")
 
-                    inputStream.bufferedReader(Charsets.UTF_8)
-                            .lines()
-                            .asSequence()
-                            .reduce { acc, s -> acc + s }
-                }
-
+private var HttpURLConnection.body: String
+    set(value) {
+        doOutput = requestMethod != "GET" && value.isNotEmpty()
+        if (doOutput)
+            DataOutputStream(outputStream).use {
+                it.writeUTF(value)
+                it.flush()
+            }
+    }
+    get() = ""
