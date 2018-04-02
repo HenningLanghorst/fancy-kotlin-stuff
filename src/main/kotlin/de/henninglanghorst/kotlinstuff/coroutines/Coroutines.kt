3521d4f4d1236873edbdac6f11f1bd9a9639c214
Henning Langhorst <henning.langhorst@googlemail.com>
2018-04-02 22:52:45 +0200

    Add Coroutines application

--- src/main/kotlin/de/henninglanghorst/kotlinstuff/coroutines/Coroutines.kt
+++ src/main/kotlin/de/henninglanghorst/kotlinstuff/coroutines/Coroutines.kt
@@ -1,65 +1,76 @@
 package de.henninglanghorst.kotlinstuff.coroutines
 
-import io.reactivex.Observable
-import io.reactivex.rxkotlin.toObservable
-import io.reactivex.schedulers.Schedulers
-import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.rx2.await
+
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
+import java.io.ByteArrayInputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.PrintStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.stream.Collectors
 
 
 fun main(args: Array<String>) {
 
-    sequential()
 
-    coroutines()
+    runBlocking {
 
-    Thread.sleep(6000)
+        println(httpRequest("https://postman-echo.com/post?hhhhh=5") {
+            requestMethod = "POST"
+            setRequestProperty("Authorization", "Basic d2lraTpwZWRpYQ==")
+            setRequestProperty("Content-Type", "application/json; charset=utf-8")
+            body = json(6, "Miller")
+        })
 
-}
+        println(httpRequest("https://postman-echo.com/get?s=5&aaa=43") {
+            requestMethod = "GET"
+            setRequestProperty("Authorization", "Basic d2lraTpwZWRpYQ==")
+        })
 
-private fun sequential() {
-    val start = System.nanoTime()
-    val shoppingCart = ShoppingCart(listOf(ArticleID(), ArticleID(), ArticleID()))
+        println(httpRequest("https://postman-echo.com/status/400") {
+            requestMethod = "GET"
+            setRequestProperty("Authorization", "Basic d2lraTpwZWRpYQ==")
+        })
 
-    val articles = shoppingCart.articleIds.map { DataStore.queryArticleByID(it) }
-    articles.forEach { println(it) }
-
-    val duration = (System.nanoTime() - start) / 1000000L
-
-    println("duration: $duration ms")
-}
-
-private fun coroutines() {
-
-    launch {
-        val start = System.nanoTime()
-        val shoppingCart = ShoppingCart(listOf(ArticleID(), ArticleID(), ArticleID()))
-
-        val articles = shoppingCart.articleIds.toObservable()
-                .flatMap { id ->
-                    Observable.defer { Observable.just(DataStore.queryArticleByID(id)) }
-                            .subscribeOn(Schedulers.io())
-                }
-                .toList()
-                .await()
-
-        articles.forEach { println(it) }
-
-        val duration = (System.nanoTime() - start) / 1000000L
-
-        println("duration: $duration ms")
 
     }
+
+
 }
 
+private fun json(id: Int, name: String) = """{"id": $id, "name": "$name"}"""
 
-object DataStore {
-    fun queryArticleByID(articleID: ArticleID): Article {
-        Thread.sleep(500)
-        return Article(articleID, "Name of $articleID")
+private suspend fun httpRequest(url: String, setup: HttpURLConnection.() -> Unit) =
+        async(CommonPool) {
+            URL(url).openConnection()
+                    .let { it as HttpURLConnection }
+                    .apply { doInput = true }
+                    .apply(setup)
+                    .run {
+                        responseStream.bufferedReader(Charsets.UTF_8)
+                                .lines()
+                                .collect(Collectors.joining(System.lineSeparator()))
+                    }
+        }.await()
+
+private val HttpURLConnection.responseStream: InputStream
+    get() =
+        try {
+            inputStream
+        } catch (e: IOException) {
+            errorStream ?: ByteArrayInputStream(byteArrayOf())
+        }
+
+private var HttpURLConnection.body: String
+    set(value) {
+        doOutput = requestMethod != "GET" && value.isNotEmpty()
+        if (doOutput)
+            PrintStream(outputStream).use {
+                it.println(value)
+                it.flush()
+            }
     }
-
-    suspend fun queryArticleByIDAsync(articleID: ArticleID) = queryArticleByID(articleID)
-}
-
-
+    get() = ""
