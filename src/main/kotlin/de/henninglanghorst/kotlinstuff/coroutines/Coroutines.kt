78f3b7cebe23d9cfc5d234960f7ea6d94267f7f0
Henning Langhorst <henning.langhorst@googlemail.com>
2018-06-04 21:29:32 +0200

    Add Coroutines application

--- src/main/kotlin/de/henninglanghorst/kotlinstuff/coroutines/Coroutines.kt
+++ src/main/kotlin/de/henninglanghorst/kotlinstuff/coroutines/Coroutines.kt
@@ -4,6 +4,7 @@ package de.henninglanghorst.kotlinstuff.coroutines
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.runBlocking
+import org.slf4j.LoggerFactory
 import java.io.ByteArrayInputStream
 import java.io.IOException
 import java.io.InputStream
@@ -13,48 +14,53 @@ import java.net.URL
 import java.util.stream.Collectors
 
 
+private val logger = LoggerFactory.getLogger(Exception().stackTrace[0].className)
+
 fun main(args: Array<String>) {
 
 
-    runBlocking {
-
-        println(httpRequest("https://postman-echo.com/post?hhhhh=5") {
-            requestMethod = "POST"
-            setRequestProperty("Authorization", "Basic d2lraTpwZWRpYQ==")
-            setRequestProperty("Content-Type", "application/json; charset=utf-8")
-            body = json(6, "Miller")
-        })
-
-        println(httpRequest("https://postman-echo.com/get?s=5&aaa=43") {
-            requestMethod = "GET"
-            setRequestProperty("Authorization", "Basic d2lraTpwZWRpYQ==")
-        })
-
-        println(httpRequest("https://postman-echo.com/status/400") {
-            requestMethod = "GET"
-            setRequestProperty("Authorization", "Basic d2lraTpwZWRpYQ==")
-        })
-
-
+    val message1 = httpRequestAsync("https://postman-echo.com/post?hhhhh=5") {
+        requestMethod = "POST"
+        setRequestProperty("Authorization", "Basic d2lraTpwZWRpYQ==")
+        setRequestProperty("Content-Type", "application/json; charset=utf-8")
+        body = json(6, "Miller")
     }
+
+    val message2 = httpRequestAsync("https://postman-echo.com/get?s=5&aaa=43") {
+        requestMethod = "GET"
+        setRequestProperty("Authorization", "Basic d2lraTpwZWRpYQ==")
+    }
+
+    val message3 = httpRequestAsync("https://postman-echo.com/status/400") {
+        requestMethod = "GET"
+        setRequestProperty("Authorization", "Basic d2lraTpwZWRpYQ==")
+    }
+
+    runBlocking {
+        println(message1.await())
+        println(message2.await())
+    }
+    runBlocking { println(message3.await()) }
 
 
 }
 
 private fun json(id: Int, name: String) = """{"id": $id, "name": "$name"}"""
 
-private suspend fun httpRequest(url: String, setup: HttpURLConnection.() -> Unit) =
-        async(CommonPool) {
-            URL(url).openConnection()
-                    .let { it as HttpURLConnection }
-                    .apply { doInput = true }
-                    .apply(setup)
-                    .run {
-                        responseStream.bufferedReader(Charsets.UTF_8)
-                                .lines()
-                                .collect(Collectors.joining(System.lineSeparator()))
-                    }
-        }.await()
+private fun httpRequestAsync(url: String, setup: HttpURLConnection.() -> Unit) = async(CommonPool) { httpRequest(url, setup) }
+
+private fun httpRequest(url: String, setup: HttpURLConnection.() -> Unit): String {
+    return URL(url).openConnection()
+            .let { it as HttpURLConnection }
+            .apply { doInput = true }
+            .apply(setup)
+            .also { logger.info("HTTP ${it.requestMethod} call to $url") }
+            .run {
+                responseStream.bufferedReader(Charsets.UTF_8)
+                        .lines()
+                        .collect(Collectors.joining(System.lineSeparator()))
+            }
+}
 
 private val HttpURLConnection.responseStream: InputStream
     get() =
@@ -74,3 +80,9 @@ private var HttpURLConnection.body: String
             }
     }
     get() = ""
+
+
+private fun return5(): Int {
+    Thread.sleep(4000)
+    return 5
+}
